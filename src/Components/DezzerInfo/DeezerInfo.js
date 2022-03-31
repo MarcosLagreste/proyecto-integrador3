ad190ba44713e6e5fed5baea9da3a6fadfce3c47
MarcosLagreste <mlagrestezucchini@udesa.edu.ar>
2022-03-31 11:07:18 -0300

    Proyecto base y limpieza

---
+++ src/Components/DezzerInfo/DeezerInfo.js
@@ -0,0 +1,11 @@
+import React from 'react'
+
+function DeezerInfo() {
+  return (
+    <div>DeezerInfo
+        {/*Hay que hacer un fetch para traer la info*/}
+    </div>
+  )
+}
+
+export default DeezerInfo
