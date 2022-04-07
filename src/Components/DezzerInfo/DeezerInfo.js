f9be9d1f9dfe0e52065b77a5223c66f491c68b12
MarcosLagreste <mlagrestezucchini@udesa.edu.ar>
2022-04-07 10:04:56 -0300

    Deezer info

--- src/Components/DezzerInfo/DeezerInfo.js
+++ src/Components/DezzerInfo/DeezerInfo.js
@@ -1,11 +1,31 @@
-import React from 'react'
+import React, {Component} from 'react'
 
-function DeezerInfo() {
-  return (
-    <div>DeezerInfo
-        {/*Hay que hacer un fetch para traer la info*/}
-    </div>
-  )
+class DeezerInfo extends Component {
+  constructor(props) {
+    super(props)
+    this.state= {
+      info: [],
+      error: ''
+    }
+  }
+
+  componentDidMount() {
+    fetch('https://thingproxy.freeboard.io/fetch/https://api.deezer.com/chart/0/tracks&top?limit=10')
+        .then((response) => response.json())
+        .then((data) => this.setState({ info: data.data }))
+        .catch((error) => this.setState({ error: 'Ups, ocurrió un error inesperado' }))
+  }
+  render(){
+    console.log('renderizado')
+    return(
+      
+      <ul>
+        {
+          this.state.info.map((info) => <li>
+            {info.title}
+          </li>)
+        }
+      </ul>
+    )}
 }
-
 export default DeezerInfo
