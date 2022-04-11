bd6394f39511848647c5e5964b5c806bfa921a80
benjalanfranco <benjalanfranco@udesa.edu.ar>
2022-04-11 09:58:06 -0300

    cargando...

--- src/Components/Tarjetas/Tarjetas.js
+++ src/Components/Tarjetas/Tarjetas.js
@@ -5,15 +5,20 @@ class Tarjetas extends Component{
   constructor(props) {
     super(props)
     this.state= {
+      isLoaded: false,
       info: [],
       error: ''
     }
   }
 
   componentDidMount() {
-    fetch('https://thingproxy.freeboard.io/fetch/https://api.deezer.com/chart/0/tracks&top?limit=10')
+    let url = 'https://thingproxy.freeboard.io/fetch/https://api.deezer.com/chart/0/tracks&top?limit=10'
+    fetch(url)
         .then((response) => response.json())
-        .then((data) => this.setState({ info: data.data }))
+        .then((data) => this.setState({
+          isLoaded: true,
+          info: data.data
+        }))
         .catch((error) => this.setState({ error: 'Ups, ocurrió un error inesperado' }))
   }
   render(){
@@ -22,8 +27,10 @@ class Tarjetas extends Component{
       
       <article>
           {
-            this.state.info.map((info) =>
-                < Tarjeta datosTarjeta={info} />
+            this.state.isLoaded === false ?
+            <p>Cargando... </p> :
+            this.state.info.map((info, idx) =>
+                < Tarjeta key={info.id + idx} datosTarjeta={info} />
             )
           }
       </article>  
